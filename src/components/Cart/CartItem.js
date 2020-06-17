d23412344e8de31a86c87d9eca802d2964b4e93d
ritesh <riteshkarwa@yahoo.com>
2020-06-16 21:33:22 -0700

    fixed warnings

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
