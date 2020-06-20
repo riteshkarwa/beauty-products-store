2fbe8b92169f59d5d2f76d9fe0b4726454a205d3
ritesh <riteshkarwa@yahoo.com>
2020-06-20 12:21:04 -0700

    add padding to show text in about page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,10 +3,12 @@ import React, { Component } from "react";
 class AboutUs extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
-            hello from about us
+      <div className="py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto text-center text-title pt-5">
+              hello from About us
+            </div>
           </div>
         </div>
       </div>
